4e8f68942670386f868f7c5799d5df5659b9e316
Craig A. Markowitz <cmarkowitz881970@gmail.com>
2019-12-25 11:56:41 -0500

    Initial Commit

---
+++ m_MusicIQ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  m_MusicIQ
+//
+//  Created by Craig Markowitz on 12/25/19.
+//  Copyright © 2019 Craig Markowitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
