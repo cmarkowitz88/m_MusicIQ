b1577f256278e3642c46093159543115a0ab3f54
Craig A. Markowitz <cmarkowitz881970@gmail.com>
2020-02-03 09:21:57 -0500

    Added a function that returns the song list from static json file

--- m_MusicIQ/GameViewController.swift
+++ m_MusicIQ/GameViewController.swift
@@ -5,12 +5,14 @@
 //  Created by Craig Markowitz on 12/25/19.
 //  Copyright © 2019 Craig Markowitz. All rights reserved.
 //
+// Download all songs from AWS: aws2 s3 cp s3://music-iq.audio /Users/craigmarkowitz/Documents/Development/m_Music_IQ/m_MusicIQ/Audio --recursive
 
 import UIKit
 
 class GameViewController: UIViewController {
 
     @IBOutlet weak var lblHello: UILabel!
+    
     let fileName = "MusicIQ_All_Songs"
     let obj_common = Common(debug_mode: true)
     
@@ -21,22 +23,27 @@ class GameViewController: UIViewController {
         
         // If we are not connected to the internet we can read the static JSON file, otherwise call AWS to get data
         if (self.obj_common.debug_mode == true){
-            self.readJsonFile()
+            let full_question_list = self.readJsonFile()
+            print(full_question_list)
         }
     }
     
-    func readJsonFile(){
+    func readJsonFile() -> FullResponse{
+        var questions:FullResponse? = nil
+
         if let path = Bundle.main.path(forResource: fileName, ofType: "json"){
             do{
                 let decoder = JSONDecoder()
                 let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-                let decoded = try decoder.decode(FullResponse.self, from: data)
-                print(decoded)
+                questions = try decoder.decode(FullResponse.self, from: data)
+               
+                print(questions)
             } catch{
                 print("error: \(error)")
             }
-            
+         
         }
+     return questions!
     }
 
 
